dcbf358644ce5cf38755e777f0e04c225c358bfa
Bill Hunter <bill@williamlhunter.com>
2024-05-08 13:32:50 -0600

    basic lsp

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- UI
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.showmode = false
+
+-- search
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
